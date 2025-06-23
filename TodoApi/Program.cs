85edd7381f2b7972d1a97b8bfdcba5a9c1fe030e
Serena Noel Vera Flores <sv267108@fi365.ort.edu.uy>
2025-06-23 17:44:09 -0300

    Make DB conexion work!!

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -2,9 +2,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext"))
-);
+// Cargar configuración manual
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
+
+
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // Add Swagger service
 builder.Services.AddControllers();
---
+++ TodoApi/TodoContextFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoApi
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
+
+    public class TodoContextFactory : IDesignTimeDbContextFactory<TodoContext>
+    {
+        public TodoContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // busca appsettings.json en el cwd
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
+            var connectionString = configuration.GetConnectionString("TodoContext");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new TodoContext(optionsBuilder.Options);
+        }
+    }
+
+}
