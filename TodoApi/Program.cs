54c951577736dfe4843277a04d014bf44fbdbc6d
Serena Noel Vera Flores <sv267108@fi365.ort.edu.uy>
2025-06-23 15:58:51 -0300

    add swagger

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,17 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder
-    .Services.AddDbContext<TodoContext>(opt =>
-        opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext"))
-    )
-    .AddEndpointsApiExplorer()
-    .AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext"))
+);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // Add Swagger service
+builder.Services.AddControllers();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();              // Enable middleware to serve generated Swagger as JSON endpoint
+    app.UseSwaggerUI();           // Enable middleware to serve Swagger UI
+}
+
+
 
 app.UseAuthorization();
 app.MapControllers();
