3b3835f2781e5102b3bbcf62a4d4d6a9563d4ae9
Serena Noel Vera Flores <sv267108@fi365.ort.edu.uy>
2025-06-23 17:44:42 -0300

    Todo Items DTOS

---
+++ TodoApi/Dtos/CreateTodoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApi.Dtos
+{
+    public class CreateTodoItem
+    {
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ TodoApi/Dtos/UpdateTodoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApi.Dtos
+{
+    public class UpdateTodoItem
+    {
+        public string Description { get; set; } = null!;
+    }
+}
