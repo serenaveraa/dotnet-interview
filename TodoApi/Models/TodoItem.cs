124e8ee64b3ba3197dcb00a26d8cbb3636955758
Serena Noel Vera Flores <sv267108@fi365.ort.edu.uy>
2025-06-23 17:44:18 -0300

    Create TodoItem.cs

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public long TodoListId { get; set; }
+        public string Description { get; set; } = null!;
+        public bool Completed { get; set; } = false;
+
+        public TodoList TodoList { get; set; } = null!;
+    }
+}
